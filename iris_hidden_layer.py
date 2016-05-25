fa0d60d1db7988231834fd35d0cff8608fea9abf
David Falcón Naranjo <david_el_125@hotmail.com>
2016-05-25 19:55:11 +0100

    refactoring

--- iris_hidden_layer.py
+++ iris_hidden_layer.py
@@ -2,6 +2,8 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 
+graph = raw_input("Show errors graph (y/n): ")
+response = str(graph).strip()
 
 # Translate a list of labels into an array of 0's and one 1.
 # i.e.: 4 -> [0,0,0,0,1,0,0,0,0,0] 
@@ -19,26 +21,32 @@ np.random.shuffle(data)
 x_data = data[:,0:4].astype('f4')
 y_data = one_hot(data[:,4].astype(int), 3)
 
-print y_data
+#print y_data
 
-print "\nSome samples..."
-for i in range(20):
-    print x_data[i], " -> ", y_data[i]
-print
-
-x = tf.placeholder("float", [None, 4])
-y_ = tf.placeholder("float", [None, 3])
+#print "\nSome samples..."
+#for i in range(20):
+#    print x_data[i], " -> ", y_data[i]
+#print
 
 
+NUMBER_OF_ENTRIES = 4
+OUTPUT_LAYER_NEURONS = 3
 
-W_2 = tf.Variable(np.float32(np.random.rand(4, 4))*0.1)
-b_2 = tf.Variable(np.float32(np.random.rand(4))*0.1)
+x = tf.placeholder("float", [None, NUMBER_OF_ENTRIES])
+y_ = tf.placeholder("float", [None, OUTPUT_LAYER_NEURONS])
 
-W = tf.Variable(np.float32(np.random.rand(4, 3))*0.1)
-b = tf.Variable(np.float32(np.random.rand(3))*0.1)
-y_2 = tf.sigmoid(tf.matmul(x, W_2) + b_2)
 
-y = tf.nn.softmax((tf.matmul(y_2, W) + b))
+HIDDEN_LAYER_NEURONS = 4
+
+hidden_W = tf.Variable(np.float32(np.random.rand(NUMBER_OF_ENTRIES, HIDDEN_LAYER_NEURONS)) * 0.1)
+hidden_b = tf.Variable(np.float32(np.random.rand(HIDDEN_LAYER_NEURONS)) * 0.1)
+
+W = tf.Variable(np.float32(np.random.rand(HIDDEN_LAYER_NEURONS, OUTPUT_LAYER_NEURONS)) * 0.1)
+b = tf.Variable(np.float32(np.random.rand(OUTPUT_LAYER_NEURONS)) * 0.1)
+
+hidden_y = tf.sigmoid(tf.matmul(x, hidden_W) + hidden_b)
+
+y = tf.nn.softmax((tf.matmul(hidden_y, W) + b))
 
 
 cross_entropy = tf.reduce_sum(tf.square(y_ - y))
@@ -57,6 +65,7 @@ print "----------------------"
 
 batch_size = 20
 
+errors = []
 for step in xrange(1000):
     for jj in xrange(len(x_data) / batch_size):
         batch_xs = x_data[jj*batch_size : jj*batch_size+batch_size]
@@ -64,14 +73,18 @@ for step in xrange(1000):
 
         sess.run(train, feed_dict={x: batch_xs, y_: batch_ys})
         if step % 50 == 0:
-            print "Iteration #:", step, "Error: ", sess.run(cross_entropy, feed_dict={x: batch_xs, y_: batch_ys})
+            error = sess.run(cross_entropy, feed_dict={x: batch_xs, y_: batch_ys})
+            errors.append(error)
+            print "Iteration #:", step, "Error: ", error
             result = sess.run(y, feed_dict={x: batch_xs})
             for b, r in zip(batch_ys, result):
                 print b, "-->", r
             print "----------------------------------------------------------------------------------"
-            
-            
-            
+
+
+if response == "y":
+    plt.plot(errors)
+    plt.show()
             
             
             
